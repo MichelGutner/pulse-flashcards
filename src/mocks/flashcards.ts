6a1e830c8e59b841465837d06460f9926a3f9cb7
Michel Gutner <michel.gutner@sanar.com>
2025-08-25 00:09:22 -0300

    feat: add mocks

---
+++ src/mocks/flashcards.ts
@@ -0,0 +1,20 @@
+export const mockFlashcards = [
+  {
+    id: "1",
+    title: "Card 1",
+    description: "Descrição do Deck 1",
+    cardCount: 10,
+  },
+  {
+    id: "2",
+    title: "Deck 2",
+    description: "Descrição do Deck 2",
+    cardCount: 5,
+  },
+  {
+    id: "3",
+    title: "Deck 3",
+    description: "Descrição do Deck 3",
+    cardCount: 8,
+  },
+];
